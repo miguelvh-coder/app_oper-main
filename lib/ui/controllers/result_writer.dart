96f1a2c1500ddf315d7ac7e74b3d4068ba1bd0fb
miguelvh-coder <72145218+miguelvh-coder@users.noreply.github.com>
2023-09-27 11:08:58 -0500

    Update result_writer.dart

--- lib/ui/controllers/result_writer.dart
+++ lib/ui/controllers/result_writer.dart
@@ -6,9 +6,15 @@ class result_writer extends GetxController {
 
   int get result => _result.value;
 
-
+  final _contador = 1.obs;
+  int get contador => _contador.value;
+  
   newdigit(int a) => _result.value = _result.value * 10 + a;
   reset() => _result.value = 0;
+
+  aumento() => _contador.value++;
+  reinicio() => _contador.value = 1;
+  
 }
 
 //LISTO
